8169b4c45e93d63bdab8c69a3e9e35ddc8f84500
Nicolas F. Rouquette <nicolas.f.rouquette@jpl.nasa.gov>
2015-06-13 20:01:03 +0000

    OTI version 835
    git-svn-id: https://www.omgwiki.org/repos/TIWG/Eclipse%20Scala%20Projects/org.omg.oti.trees@836 b05f3425-8805-43f2-8a1d-5852a8693556

--- project/Versions.scala
+++ project/Versions.scala
@@ -20,7 +20,7 @@ object Versions {
   // OTI Core version
     
   val oti_core_prefix = "0.14.0"
-  val oti_core_suffix = "823"
+  val oti_core_suffix = "833"
   val oti_core_version = oti_core_prefix+"-"+oti_core_suffix
 
 }
