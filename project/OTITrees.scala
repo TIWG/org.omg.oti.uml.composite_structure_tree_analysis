f83f0da461a8634de31c048cc20cee4bd16fff49
Nicolas F. Rouquette <nicolas.f.rouquette@jpl.nasa.gov>
2015-06-15 17:32:08 +0000

    OTI version 861 improved resource publishing
    git-svn-id: https://www.omgwiki.org/repos/TIWG/Eclipse%20Scala%20Projects/org.omg.oti.trees@862 b05f3425-8805-43f2-8a1d-5852a8693556

--- project/OTITrees.scala
+++ project/OTITrees.scala
@@ -60,7 +60,13 @@ object OTITrees extends Build {
         unmanagedResourceDirectories in Compile ~= { _.filter(_.exists) },
         unmanagedResourceDirectories in Test ~= { _.filter(_.exists) }
       )
-      
+
+  def addIfExists(f: File, name: String): Seq[(File, String)] =
+    if (!f.exists) Seq()
+    else Seq((f, name))
+
+  val QUALIFIED_NAME = "^[a-zA-Z][\\w_]*(\\.[a-zA-Z][\\w_]*)*$".r
+
   lazy val oti_trees = Project(
     "oti-trees",
     file(".")).
@@ -81,21 +87,34 @@ object OTITrees extends Build {
       scalaSource in Compile := baseDirectory.value / "src",
       classDirectory in Compile := baseDirectory.value / "bin",
 
-      com.typesafe.sbt.packager.Keys.topLevelDirectory in Universal := Some("dynamicScripts/org.omg.oti.trees"),
-      mappings in Universal <++= (baseDirectory, packageBin in Compile, packageSrc in Compile, packageDoc in Compile) map {
-        (dir, bin, src, doc) =>
-          (dir ** "*.dynamicScripts").pair(relativeTo(dir)) ++
-            (dir ** "*.md").pair(relativeTo(dir)) ++
-            com.typesafe.sbt.packager.MappingsHelper.directory(dir / "resources") ++
-            Seq(
-              (bin, "lib/" + bin.name),
-              (src, "lib.sources/" + src.name),
-              (doc, "lib.javadoc/" + doc.name)
-            )
+      // the '*-resource.zip' archive will start from: 'dynamicScripts/<dynamicScriptsProjectName>'
+      com.typesafe.sbt.packager.Keys.topLevelDirectory in Universal := {
+        val dynamicScriptsProjectName = baseDirectory.value.getName
+        require(
+          QUALIFIED_NAME.pattern.matcher(dynamicScriptsProjectName).matches,
+          s"The project name, '$dynamicScriptsProjectName` is not a valid Java qualified name")
+        Some("dynamicScripts/" + dynamicScriptsProjectName)
       },
-      artifacts <+= (name in Universal) { n => Artifact(n, "jar", "jar", Some("resource"), Seq(), None, Map()) },
-      packagedArtifacts <+= (packageBin in Universal, name in Universal) map { (p,n) =>
-        Artifact(n, "jar", "jar", Some("resource"), Seq(), None, Map()) -> p
+
+      // name the '*-resource.zip' in the same way as other artifacts
+      com.typesafe.sbt.packager.Keys.packageName in Universal :=
+        normalizedName.value + "_" + scalaBinaryVersion.value + "-" + version.value + "-resource",
+
+      // contents of the '*-resource.zip' to be produced by 'universal:packageBin'
+      mappings in Universal <++= (baseDirectory,
+        packageBin in Compile, packageSrc in Compile, packageDoc in Compile,
+        packageBin in Test, packageSrc in Test, packageDoc in Test) map {
+        (dir, bin, src, doc, binT, srcT, docT) =>
+          (dir ** "*.dynamicScripts").pair(relativeTo(dir)) ++
+            ((dir ** "*.md") --- (dir / "sbt.staging" ***)).pair(relativeTo(dir)) ++
+            (dir / "models" ** "*.mdzip").pair(relativeTo(dir)) ++
+            com.typesafe.sbt.packager.MappingsHelper.directory(dir / "resources") ++
+            addIfExists(bin, "lib/" + bin.name) ++
+            addIfExists(binT, "lib/" + binT.name) ++
+            addIfExists(src, "lib.sources/" + src.name) ++
+            addIfExists(srcT, "lib.sources/" + srcT.name) ++
+            addIfExists(doc, "lib.javadoc/" + doc.name) ++
+            addIfExists(docT, "lib.javadoc/" + docT.name)
       },
 
       aether.AetherKeys.aetherArtifact <<=
@@ -106,7 +125,6 @@ object OTITrees extends Build {
           (coords: aether.MavenCoordinates, mainArtifact: File, pom: File, artifacts: Map[Artifact, File]) =>
             aether.AetherPlugin.createArtifact(artifacts, pom, coords, mainArtifact)
         },
-
 
       shellPrompt := { state => Project.extract(state).currentRef.project + " @ " + Versions.version_suffix + "> " }
     )
