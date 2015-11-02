7dccc0f23eafcdbd08c4d9b46574d1899c3e2cfc
Nicolas F. Rouquette <nicolas.f.rouquette@jpl.nasa.gov>
2015-11-02 01:16:24 +0000

    OTI/Core 445104 (0.19)
    git-svn-id: https://www.omgwiki.org/repos/TIWG/Eclipse%20Scala%20Projects/org.omg.oti.trees@445106 b05f3425-8805-43f2-8a1d-5852a8693556

--- project/Versions.scala
+++ project/Versions.scala
@@ -20,7 +20,7 @@ object Versions {
   // OTI Core version
     
   val oti_core_prefix = version_prefix
-  val oti_core_suffix = "445088"
+  val oti_core_suffix = "445104"
   val oti_core_version = oti_core_prefix+"-"+oti_core_suffix
 
 }
